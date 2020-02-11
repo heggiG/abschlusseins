716bb11c84d15262287401021a9c5e536092a575
heggiG <florianheck0@gmail.com>
2020-02-11 11:51:42 +0100

    updated point and command

--- src/modeltrain/Point.java
+++ src/modeltrain/Point.java
@@ -18,6 +18,16 @@ public class Point {
         return this.yCord;
     }
     
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point))
+            return false;
+        if (o.toString().equals(this.toString()))
+            return true;
+        return false;
+    }
+    
+    @Override
     public String toString() {
         return "(" + xCord + "),(" + yCord + ")";
     }
