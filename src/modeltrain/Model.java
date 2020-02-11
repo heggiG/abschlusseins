9f6127f0a79f2d6afa9500c69bf774430f4fff75
heggiG <florianheck0@gmail.com>
2020-02-11 15:52:02 +0100

    UpdateJonas

--- src/modeltrain/Model.java
+++ src/modeltrain/Model.java
@@ -10,5 +10,8 @@ public class Model {
     
     private Set<Track> tracks;
     
+    public Model() {
+        tracks = new HashSet<>();
+    }
     
 }
--- src/modeltrain/commands/AddTrack.java
+++ src/modeltrain/commands/AddTrack.java
@@ -5,17 +5,15 @@ import java.util.regex.MatchResult;
 
 public class AddTrack extends Command {
 
-    public static final String REGEX = "add track " + COORDINATE;
-    private MatchResult mr;
+    private static final String REGEX = "add track " + COORDINATE;
     
-    public AddTrack(Model model, String input) {
+    public AddTrack(Model model) {
         super(model, REGEX);
-        mr = getMatcher(input);
     }
     
     @Override
     public void execute(String command) {
-        String coord = mr.group(1);
+        MatchResult mr = getMatcher(command);
     }
 
 }
