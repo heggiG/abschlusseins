a965dcbfdb560b2a647f72e756eb306dbcff696e
heggiG <florianheck0@gmail.com>
2020-02-11 15:25:53 +0100

    UpdateJonas

--- src/modeltrain/ui/Main.java
+++ src/modeltrain/ui/Main.java
@@ -1,22 +1,15 @@
 package modeltrain.ui;
 
 import modeltrain.Model;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import edu.kit.informatik.Terminal;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String[] args) {
         Model model = new Model();
         UserInterface userInterface = new UserInterface(model);
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Model successfully initialized");
-
         do {
-            userInterface.executeCommand(reader.readLine());
+            userInterface.executeCommand(Terminal.readLine());
         } while (!userInterface.isQuit());
     }
 }
