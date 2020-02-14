e724010f67cda3d5ee33896a3ba1dff2bf854961
heggiG <florianheck0@gmail.com>
2020-02-15 00:52:37 +0100

    added widen and translate

--- src/modeltrain/core/TrackNetworkAlt.java
+++ src/modeltrain/core/TrackNetworkAlt.java
@@ -8,16 +8,31 @@ import modeltrain.trains.Train;
 public class TrackNetworkAlt {
 
     @SuppressWarnings("unused")
-    private Object[][][] tracks;
+    private int[][][] tracks;
     @SuppressWarnings("unused")
     private Set<Train> trains;
-    
+    int translatedLength;
+    int translatedHeight;
+
     public TrackNetworkAlt() {
-        tracks = new Object[1][1][2];
+        tracks = new int[1][1][2];
         trains = new HashSet<>();
+        translatedHeight = 0;
+        translatedLength = 0;
     }
-    
-    public static int[][][] translate(int x, int y, int[][][] old) {
+
+    /**
+     * Translates the track array by a negative value to compensate negative inputs
+     * 
+     * @param x X value to translate by
+     * @param y Y value to translate by
+     * @param old the array to translate
+     * @return the translated array
+     */
+    private int[][][] translate(int x, int y, int[][][] old) {
+        if (x > 0 || y > 0) {
+            throw new IllegalArgumentException("method is meant to translate by negative numbers");
+        }
         int[][][] newArray = new int[old.length + x][old[0].length + y][old[0][0].length];
         for (int i = 0; i < old.length; i++) {
             for (int j = 0; j < old[0].length; j++) {
@@ -26,7 +41,33 @@ public class TrackNetworkAlt {
                 }
             }
         }
+        this.translatedLength = x;
+        this.translatedHeight = y;
         return newArray;
     }
-    
+
+    /**
+     * Widens an array to allow index inputs greater than {@code array.length}
+     * 
+     * @param x x value tried to input
+     * @param y y value tried to input
+     * @param old the array to widen
+     * @return the widened array
+     */
+    private int[][][] widen(int x, int y, int[][][] old) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException("method is meant to widen an array by positive integers");
+        }
+        int[][][] returnArray = new int[old.length > x ? old.length : x + 1]
+                [old[0].length > y ? old.length : y + 1][old[0][0].length];
+        for (int i = 0; i < old.length; i++) {
+            for (int j = 0; j < old[0].length; j++) {
+                for (int k = 0; k < old[0][0].length; k++) {
+                    returnArray[i][j][k] = old[i][j][k];
+                }
+            }
+        }
+        return returnArray;
+    }
+
 }
--- src/modeltrain/ui/Main.java
+++ src/modeltrain/ui/Main.java
@@ -8,25 +8,11 @@ import modeltrain.core.Model;
 public class Main {
     
     public static void main(String[] args) {
-      /*  Model model = new Model();
+        Model model = new Model();
         UserInterface userInterface = new UserInterface(model);
         do {
             userInterface.executeCommand(Terminal.readLine());
-        } while (!userInterface.isExit()); */
+        } while (!userInterface.isExit()); 
         
-        int[][][] n = new int[2][2][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 2; k++) {
-                    n[i][j][k] = i + j + k;
-                }
-            }
-        }
-        
-        n = modeltrain.core.TrackNetworkAlt.translate(2, 2, n);
-        
-        for(int k[][] : n) {
-                Terminal.printLine(Arrays.toString(k));
-        }
     }
 }
